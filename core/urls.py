a475e224eeba3cca69e80f3dad359e97e7f4d926
djumanov <diyorbekjumanov1120@gmail.com>
2023-05-11 19:32:27 +0500

    views module

--- core/urls.py
+++ core/urls.py
@@ -1,28 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpRequest, HttpResponse, JsonResponse
-
-
-def home(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('<h1>Hello</h1>')
-
-def get_params(request: HttpRequest):
-    params = request.GET
-    number = params.get('number')
-    print(number)
-    pass
-
-def get_sum(request: HttpRequest):
-    params = request.GET
-
-    a = params.get('a', 0)
-    b = params.get('b', 0)
-
-    result = {
-        'result': int(a) + int(b)
-    }
-    
-    return JsonResponse(result)
+from .views import (
+    home,
+    get_params,
+    get_sum
+)
 
 
 urlpatterns = [
---
+++ core/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpRequest, HttpResponse, JsonResponse
+
+
+def home(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('<h1>Hello</h1>')
+
+def get_params(request: HttpRequest):
+    params = request.GET
+    number = params.get('number')
+    print(number)
+    pass
+
+def get_sum(request: HttpRequest):
+    params = request.GET
+
+    a = params.get('a', 0)
+    b = params.get('b', 0)
+
+    result = {
+        'result': int(a) + int(b)
+    }
+    
+    return JsonResponse(result)
