4c5e24b403fc71ce67cefe824b6a6ed338b0d6bc
djumanov <diyorbekjumanov1120@gmail.com>
2023-05-11 18:59:06 +0500

    task

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpRequest
 
 
-def home(request):
+def home(request: HttpRequest):
     print(request.method)
+    print(request.path)
+    print(request.content_type)
+    pass
+
+def get_params(request: HttpRequest):
+    params = request.GET
+    number = params.get('number')
+    print(number)
+    pass
+
+def get_sum(request: HttpRequest):
     pass
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home),
+    path('get-num/', get_params),
+    path('get-sum/', get_sum),
 ]
